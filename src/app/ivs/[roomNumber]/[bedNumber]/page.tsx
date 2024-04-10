4f6ec2366eca6a56d72e1361ea53ef2158df2c39
pulkit4603 <pulkitdwivedi123@gmail.com>
2024-04-11 01:59:15 +0530

    fix: bed page fixed

--- src/app/ivs/[roomNumber]/[bedNumber]/page.tsx
+++ src/app/ivs/[roomNumber]/[bedNumber]/page.tsx
@@ -17,29 +17,48 @@ export default function page({params,} : {params: {bedNumber: string, roomNumber
 
     return (
         <>
-        <div className="flex flex-col items-start justify-between p-4 md:p-4">
-            <CustomBreadcrumb items={breadcrumbItems} />
-        </div>
-        
-        <div className="flex flex-col items-start justify-start border border-black">
-            <div>
-                <div className="text-2xl">Bed Number: {params.bedNumber}</div>
-                <div className="text-2xl">Room Number: {params.roomNumber}</div>
-                <div className="text-2xl">Patient Name: {bedData?.patientName}</div>
-            </div>
+            <div className="flex flex-col items-start justify-start rounded-lg">
+                <div className="w-full min-w-full text-md p-4 text-lg border-b">
+                    <p>CONTROL PAGE</p>
+                </div>
 
-            <div className="flex items-start justify-start border border-red">
-                <div className="w-48 h-48 p-6 border border-black">
+                <div className="flex flex-grow items-center justify-between border-b">
+                    <div className="min-w-48 w-48 h-48 p-2">
                     <Image 
                         src="/patientPhoto.jpg"
                         alt="Patient Photo"
                         width={16}
                         height={16}
-                        layout="responsive" 
+                        layout="responsive"
                     />
+                    </div>
+                    <div className="flex-grow flex-shrink p-6">
+                    <h2 className="text-md font-bold">About Patient</h2>
+                    <p className="text-sm mb-4">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                        Nostrum aut reprehenderit minus vitae, laudantium iste nulla 
+                        sequi accusantium, sit consectetur velit eaque. 
+                        Repudiandae, reprehenderit doloribus. Asperiores debitis ad 
+                        odit iusto! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Libero necessitatibus nemo molestiae impedit?
+                    </p>
+                    <div className="flex space-x-4 justify-between">
+                        <div>
+                        <h3 className="text-sm font-bold">Patient Name</h3>
+                        <p className="text-sm">John Doe</p>
+                        </div>
+                        <div>
+                        <h3 className="text-sm font-bold">Phone Number</h3>
+                        <p className="text-sm">+91 234 567 8901</p>
+                        </div>
+                        <div>
+                        <h3 className="text-sm font-bold">Address</h3>
+                        <p className="text-sm">123 Main St, Anytown, USA</p>
+                        </div>
+                    </div>
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
